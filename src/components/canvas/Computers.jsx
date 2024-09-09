fb9b28335101ed0e8acdc1a0c92c73abbe2fbcfa
Umayerk <umayer.k2004@gmail.com>
2024-09-09 14:56:08 -0400

    first commit

---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,84 @@
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
+
+const Computers = ({ isMobile, isTablet }) => {
+  const computer = useGLTF("./desktop_pc/scene.gltf", GLTFLoader);
+
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={2} />
+      <primitive
+        object={computer.scene}
+        // Adjust scale: mobile (7.9), tablet (4.9), desktop (17.0)
+        scale={isMobile ? 79.9 : isTablet ? 1.9 : 17.0}
+        position={isMobile ? [-2, -1.5, 0] : [0, -2.5, 0]} // Adjust position for better alignment on mobile/tablet
+        rotation={[0, 40, 0]} // Adjust rotation for better alignment with the camera
+      />
+    </mesh>
+  );
+};
+
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  const [isTablet, setIsTablet] = useState(false);
+
+  useEffect(() => {
+    const mobileQuery = window.matchMedia("(max-width: 500px)");
+    const tabletQuery = window.matchMedia("(min-width: 501px) and (max-width: 1024px)");
+
+    setIsMobile(mobileQuery.matches);
+    setIsTablet(tabletQuery.matches);
+
+    const handleMobileQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    const handleTabletQueryChange = (event) => {
+      setIsTablet(event.matches);
+    };
+
+    mobileQuery.addEventListener("change", handleMobileQueryChange);
+    tabletQuery.addEventListener("change", handleTabletQueryChange);
+
+    return () => {
+      mobileQuery.removeEventListener("change", handleMobileQueryChange);
+      tabletQuery.removeEventListener("change", handleTabletQueryChange);
+    };
+  }, []);
+
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} isTablet={isTablet} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
